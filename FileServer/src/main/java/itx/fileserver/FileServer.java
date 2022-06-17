2cd85ab9688485434315e267426f4bcbe4eb3f7e
sungilkim <darwinkim@gmail.com>
2022-06-17 10:34:02 +0900

    main4

--- FileServer/src/main/java/itx/fileserver/FileServer.java
+++ FileServer/src/main/java/itx/fileserver/FileServer.java
@@ -38,7 +38,7 @@ public class FileServer {
     }
 
     public static void main(String[] args) {
-        LOG.info("Spring file server main3");
+        LOG.info("Spring file server main4");
         FileServer fileServer = new FileServer();
         fileServer.start(args);
     }
