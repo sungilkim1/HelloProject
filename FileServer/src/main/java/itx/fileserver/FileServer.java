18bab3a7503c04a577d5ce84904b55708cc54cd7
sungilkim1 <80303046+sungilkim1@users.noreply.github.com>
2022-06-17 11:12:33 +0900

    main9

--- FileServer/src/main/java/itx/fileserver/FileServer.java
+++ FileServer/src/main/java/itx/fileserver/FileServer.java
@@ -39,7 +39,7 @@ public class FileServer {
     }
 
     public static void main(String[] args) {
-        LOG.info("Spring file server main8");
+        LOG.info("Spring file server main9");
         FileServer fileServer = new FileServer();
         fileServer.start(args);
     }
