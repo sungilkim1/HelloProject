c1a5da75b239701ea77f3339d697e22b97d10ec7
sungilkim1 <80303046+sungilkim1@users.noreply.github.com>
2022-06-17 10:45:41 +0900

    Update FileServer.java
    main7

--- FileServer/src/main/java/itx/fileserver/FileServer.java
+++ FileServer/src/main/java/itx/fileserver/FileServer.java
@@ -38,7 +38,7 @@ public class FileServer {
     }
 
     public static void main(String[] args) {
-        LOG.info("Spring file server main6");
+        LOG.info("Spring file server main7");
         FileServer fileServer = new FileServer();
         fileServer.start(args);
     }
